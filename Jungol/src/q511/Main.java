480314976e3d19f7e1ddc09a7bc40e048a348018
Class01 <Class01@DESKTOP-UR65MFL>
2023-04-26 17:59:19 +0900

    04.26

--- Jungol/src/q511/Main.java
+++ Jungol/src/q511/Main.java
@@ -1,14 +1,15 @@
 package q511;
 
 public class Main {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x = 45;
-		int y = 9;
+		int x = 9;
+		int y = 5;
 		
-		System.out.println("55 - 10 = "+x);
-		System.out.println("2008 - 1999 = "+y);
+		System.out.printf("55 - 10 = ");
+		System.out.println(x*y);
+		System.out.printf("2008 - 1999 = ");
+		System.out.print(x);
 	}
 
 }
