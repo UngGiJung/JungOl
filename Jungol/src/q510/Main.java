dcef1b325f016d0ab49011da6492ba164376e493
Class01 <Class01@DESKTOP-UR65MFL>
2023-04-26 17:49:16 +0900

    04.26

---
+++ Jungol/src/q510/Main.java
@@ -0,0 +1,15 @@
+package q510;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = -1;
+		int y = 100;
+		
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+
+}
