dc2dabcecb6074a4876ee79c2af9d31a598795e0
Class01 <Class01@DESKTOP-UR65MFL>
2023-04-27 16:39:37 +0900

    04.27

---
+++ Jungol/src/q513/Main.java
@@ -0,0 +1,24 @@
+package q513;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double yd = 91.44;
+		double in = 2.54;
+		
+		double x = 2.1;
+		double y = 10.5;
+		
+		System.out.print(" ");
+		System.out.print(x+"yd = ");
+		System.out.printf("%.1f",(x*yd));
+		System.out.println("cm");
+		
+		System.out.print(y+"in =  ");
+		System.out.printf("%.1f",(y*in));
+		System.out.print("cm");
+
+	}
+
+}
