bddf1341f9418b1b70df809e4b0bac899bd495a8
La_Ung <La_Ung@La_UNG_HomeDeskTopPC>
2023-04-28 01:18:24 +0900

    04.27 home

---
+++ Jungol/src/q9018/Main.java
@@ -0,0 +1,28 @@
+package q9018;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 7;
+		int y = 5;
+		
+		System.out.print("두 개의 수를 입력하시오. ");
+		
+		int d1 = x + y;
+		System.out.println("7 + 5 = "+d1);
+		
+		int d2 = x - y;
+		System.out.println("7 - 5 = "+d2);
+		
+		int d3 = x * y;
+		System.out.println("7 * 5 = "+d3);
+		
+		int d4 = x / y;
+		System.out.println("7 / 5 = "+d4);
+		
+		int d5 = x % y;
+		System.out.print("7 % 5 = "+d5);	
+	}
+
+}
