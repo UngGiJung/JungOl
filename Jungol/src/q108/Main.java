e0622feaca74b991f3f14f8967c8e7b44d0a4476
Class01 <Class01@DESKTOP-UR65MFL>
2023-04-27 17:56:03 +0900

    04.27

---
+++ Jungol/src/q108/Main.java
@@ -0,0 +1,13 @@
+package q108;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 50;
+		double y = 100.12;
+		
+		System.out.printf(y+" * "+x+" = "+"%4.0f",(x*y));
+	}
+
+}
--- Jungol/src/q513/Main.java
+++ Jungol/src/q513/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//케이스1
 		double yd = 91.44;
 		double in = 2.54;
 		
@@ -18,6 +20,15 @@ public class Main {
 		System.out.print(y+"in =  ");
 		System.out.printf("%.1f",(y*in));
 		System.out.print("cm");
+		
+		//케이스2
+//		int yd= 1;
+//		int in= 1;
+//		double c=91.44;
+//		double m =2.54;
+//			      
+//		System.out.printf(" %.1fyd = %.1fcm\n",2.1*yd ,2.1*c);
+//		System.out.printf("%.1fin =  %.1fcm\n",10.5*in,10.5*m);
 
 	}
 
