e0ef5fc17e30b96af28da9a5406131e6f69ab09c
La_Ung <La_Ung@La_UNG_HomeDeskTopPC>
2023-04-28 00:26:15 +0900

    04.27 home

---
+++ Jungol/src/q9013/Main.java
@@ -0,0 +1,20 @@
+package q9013;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x = 1.2340;
+		double y = 10.3459;
+		
+		System.out.print("전체 7자리로 맞추고 소수 4자리까지 출력\n");
+		System.out.printf("x = %7.4f\n",x);
+		System.out.printf("y = %7.4f\n\n",y);
+		
+		System.out.print("소수 2자리까지 출력(반올림)\n");
+		System.out.printf("x = %1.2f\n",x);
+		System.out.printf("y = %1.2f",y);
+
+	}
+
+}
