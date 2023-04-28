73201fb626ccf2008987f648acc5eaf38663bf6f
Class01 <Class01@DESKTOP-UR65MFL>
2023-04-28 18:04:26 +0900

    04.28

---
+++ Jungol/src/q536/Main.java
@@ -0,0 +1,31 @@
+package q536;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. for문
+		for(int i=1;i<=15;i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		
+		// 2. while문
+		int j = 1;
+		while (j < 16) {
+			System.out.print(j++ + " ");
+			//j++
+		}
+		System.out.println();
+		
+		
+		
+		// 3. do ~ while 문
+		int k = 1;
+		do {
+			System.out.print(k++ + " ");
+		}while(k<16);
+		
+	}
+
+}
