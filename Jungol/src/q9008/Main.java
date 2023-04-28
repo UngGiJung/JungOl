ef1e6eb36a064e15eaf35e9d388d7056e4a85dc6
Class01 <Class01@DESKTOP-UR65MFL>
2023-04-28 09:45:47 +0900

    04.27

---
+++ Jungol/src/q9008/Main.java
@@ -0,0 +1,11 @@
+package q9008;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf(" subject score\n"+"==============\n");
+        System.out.printf("%8s%6s\n%8s%6s\n%8s%6s","korean","90","english","100","computer","80");
+	}
+
+}
