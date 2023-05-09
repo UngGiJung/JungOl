9eecd17c8656d253633ed7d90b5b96074a580143
Class01 <Class01@DESKTOP-UR65MFL>
2023-05-09 18:24:50 +0900

    05.09

--- Jungol/src/q1291/Main.java
+++ Jungol/src/q1291/Main.java
@@ -7,18 +7,23 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
+		int x,y;
 		
 		while (true) {
-			int x = sc.nextInt();
-			int y = sc.nextInt();
-			
-			if ( x < 2 || x > 9 || y < 2 || y > 9) {
-				System.out.print("INPUT ERROR!");
-				continue;	
-			}
+			x = sc.nextInt();
+			y = sc.nextInt();
 
-		if (x < y && x > 1) {
+			if (x < 2 || x > 9 || y < 2 || y > 9) {
+				System.out.println("INPUT ERROR!");
+				continue;
+			} else {
+				sc.close();
+				break;
+			}
+			
+		}
+		
+		if (x < y) {
 			for (int m1 = 1; m1 <= 9; m1++) {
 				for (int n1 = x; n1 <= y; n1++) {
 					System.out.printf(n1 + " * " + m1 + " = %2d", (n1 * m1));
@@ -26,7 +31,7 @@ public class Main {
 				}
 				System.out.println();
 			}
-		} else if (x > y && y > 1) {
+		} else if (x >= y) {
 			for (int m2 = 1; m2 <= 9; m2++) {
 				for (int n2 = x; n2 >= y; n2--) {
 					System.out.printf(n2 + " * " + m2 + " = %2d", (n2 * m2));
@@ -34,10 +39,7 @@ public class Main {
 				}
 				System.out.println();
 			}
-		} //else {
-//			System.out.println("INPUT ERROR!");
-//		}
-		sc.close();
+		}
+
 	}
-}
 }
