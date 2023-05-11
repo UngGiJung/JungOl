38f674c6c3ce51c6137961f7e97f92da9aef5ae8
La_Ung <La_Ung@La_UNG_HomeDeskTopPC>
2023-05-11 21:54:59 +0900

    05.11 home

--- Jungol/src/q558/Main.java
+++ Jungol/src/q558/Main.java
@@ -7,20 +7,25 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-//		int x = sc.nextInt();
-//		sc.close();
 		int[] a = new int[100];
-
 		for (int i = 0; i <= a.length; i++) {
 			a[i] = sc.nextInt();
-			System.out.print(a[i] + " ");
-			
+
+//			System.out.print(a[i] + " ");
+
 			if (a[i] == 0) {
-				for (int j = a.length; j > 0; j--) {
+				for (int j = i-1; j >= 0; j--) {
+//						a[i] = null
+//						a[0] = 3;
+//						a[1] = 5;
+//						a[2] = 10;
+//						a[3] = 55;
+//						a[4] = 0;
 //					a[j] = sc.nextInt();
-					System.out.print(a[j]);
+					System.out.print(a[j] + " ");
 				}
 			}
 		}
+		sc.close();
 	}
 }
